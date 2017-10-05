a9a86ce52693c2d9840a22f367a55fd13351ffaf
Ghazi Ben Ahmed <ghazi@Ghazis-MacBook-Pro.local>
2017-10-05 14:05:07 +0200

    add kafka producer callback

--- src/main/scala/KafkaProducer.scala
+++ src/main/scala/KafkaProducer.scala
@@ -2,6 +2,7 @@ import java.util.concurrent.Future
 import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Callback
 import scala.io.Source
 
 object KafkaProducer extends App {
@@ -18,7 +19,13 @@ object KafkaProducer extends App {
   for (line <- Source.fromResource(filename).getLines) {
     i += 1
     val record = new ProducerRecord[String, String]("logs","line-"+i , line)
-    val metaF: Future[RecordMetadata] = producer.send(record)
+    val metaF: Future[RecordMetadata] = producer.send(record, new Callback{
+      override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+        if (exception != null) {
+          println(exception.toString)
+        }
+      }
+    })
     val meta = metaF.get()
   }
   producer.close()
