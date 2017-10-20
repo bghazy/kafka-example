cd4e55b63066dd065529efc85d410fa8282daf0d
Ghazy Ben Ahmed <ghazy.ben.ahmed@gmail.com>
2017-10-20 14:39:20 +0200

    fix streamer code

--- src/main/scala/KafkaStreamer.scala
+++ src/main/scala/KafkaStreamer.scala
@@ -1,5 +1,6 @@
 import java.util.Properties
 import java.util.regex.Pattern
+import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.kstream._
 import org.apache.kafka.common.serialization.{Serdes, StringDeserializer, StringSerializer, _}
 import org.apache.kafka.streams.KafkaStreams
@@ -9,14 +10,15 @@ object KafkaStreamer extends App {
   val  props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("zookeeper.connect", "localhost:2181")
-   props.put("key.deserializer", new StringDeserializer)
-  props.put("value.deserializer", new StringDeserializer)
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
   props.put("group.id", "something")
   props.put("application.id", "logs")
 
   val builder: KStreamBuilder = new KStreamBuilder
   val stringSerde: Serde[String] = Serdes.serdeFrom(new StringSerializer, new StringDeserializer)
   val longSerde: Serde[java.lang.Long] = Serdes.serdeFrom(new LongSerializer,new LongDeserializer)
+  val byteArraySerde : Serde[Array[Byte]] = Serdes.serdeFrom(new ByteArraySerializer, new ByteArrayDeserializer)
   val logs: KStream[String, String] = builder.stream(stringSerde, stringSerde, "logs")
   val records: KStream[String, (String,String,String)] = logs.mapValues{
     record : String=>{
@@ -37,11 +39,16 @@ object KafkaStreamer extends App {
     level => {
       records.filter(new Predicate[String, (String,String,String)] {
         override def test(key: String, value: (String,String,String)): Boolean = value._2 == level
-      }).groupBy[String](new KeyValueMapper[String,(String,String,String),String]() {
-        override def apply(key: String, value: (String,String,String)): (String) = value._1
-      }).count(level).toStream.to(stringSerde, longSerde, level)
+      }).map [String, Integer]{new KeyValueMapper[String, (String,String,String), KeyValue[String, Integer]] {
+        override def apply(key: String, value: (String,String,String)): KeyValue[String, Integer] = {
+          new KeyValue(value._1, 1)
+        }
+      }}.groupByKey(Serdes.String, Serdes.Integer).reduce(new Reducer[Integer] {
+        override def apply(value1: Integer, value2: Integer): Integer = value1 + value2
+      },level).to(level)
     }
   }
+
 
   val streams = new KafkaStreams(builder, props)
   streams.start()
